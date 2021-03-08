4f589d4cd825d74b4f568d6dc1660f499fe132b6
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2021-03-08 01:04:41 +0100

    ddddddddd

--- lekcja01/src/main/java/DateServlet.java
+++ lekcja01/src/main/java/DateServlet.java
@@ -32,6 +32,9 @@ public class DateServlet extends HttpServlet {
                 .append("<iframe width=\"420\" height=\"315\"\n" +
                         "src=\"https://www.youtube.com/embed/tgbNymZ7vqY\">\n" +
                         "</iframe>")
+                .append("<br/>")
+                .append("<br/>")
+                .append("<br/>")
                 .append("</body></html>");
         responseOutput.append("<html>\n" +
                 "<title>dddddddddd</title>\n" +
