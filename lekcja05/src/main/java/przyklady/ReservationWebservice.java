fe83e2518397ef0e12e28fa987779b5dbf373b15
Mati <55853886+bozo1209@users.noreply.github.com>
2021-03-16 00:05:04 +0100

    test

--- lekcja05/src/main/java/przyklady/ReservationWebservice.java
+++ lekcja05/src/main/java/przyklady/ReservationWebservice.java
@@ -7,6 +7,7 @@ import javax.ws.rs.core.Response;
 @Path("/reservation")
 public class ReservationWebservice {
 
+//    test
     @GET
     public Response listReservations(){
         return Response.ok("wszystkie rezerwacje: ").build();
